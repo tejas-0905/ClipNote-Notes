e96e5731840cafdb17c57d743ee410a055b8983a
Tejas Jiddewar <145433955+tejas-0905@users.noreply.github.com>
2025-06-24 23:05:43 +0530

    Apply light mode background class to body if theme is not 'dark'

--- script.js
+++ script.js
@@ -10,3 +10,5 @@ if (mode === 'dark') {
 body.classList.remove('bg-green-100');
 body.classList.add('bg-gray-900', 'text-white');
 toggleBtn.textContent = 'light_mode';
+} else {
+    body.classList.add('bg-green-100');
