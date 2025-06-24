14ba4aa0c4036827dc74278225db07e1bb0e922f
Tejas Jiddewar <145433955+tejas-0905@users.noreply.github.com>
2025-06-24 22:43:40 +0530

    Select notes container element using getElementById for dynamic note rendering

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const addBtn = document.getElementById('addBtn');
 const toggleBtn = document.getElementById('toggleMode');
 const gridBtn = document.getElementById('gridView');
 const listBtn = document.getElementById('listView');
+const notesContainer = document.getElementById('notesContainer');
