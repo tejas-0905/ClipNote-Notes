7329b79d0683b6876d40cc2a3d2fbb2ae7ca9a12
Tejas Jiddewar <145433955+tejas-0905@users.noreply.github.com>
2025-06-24 23:08:58 +0530

    Apply Tailwind grid layout classes to notesContainer if layout is set to 'grid'

--- script.js
+++ script.js
@@ -24,3 +24,6 @@ toggleBtn.addEventListener('click', () => {
   setTheme(newTheme);
 });
 const setLayout = (layout) => {
+if (layout === 'grid') {
+    notesContainer.className = "grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5";
+  }
