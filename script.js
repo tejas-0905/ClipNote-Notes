97d88590b2a704135d24475d45fa0cf626c346c4
Tejas Jiddewar <145433955+tejas-0905@users.noreply.github.com>
2025-06-24 22:43:18 +0530

    Select 'List View' button using getElementById for layout switching

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const addBtn = document.getElementById('addBtn');
 const toggleBtn = document.getElementById('toggleMode');
 const gridBtn = document.getElementById('gridView');
+const listBtn = document.getElementById('listView');
